a90419a3932ea5b05f37a26d4269bb4e9c002090
Fabian Große <fabian-grosse@t-online.de>
2018-10-12 16:47:37 +0200

    fixed "theme_switch" reference error

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ document.addEventListener("scroll", function() {
 /* #### Dark/Night theme on desktop #### */
 document.addEventListener("DOMContentLoaded", function(event) {
     // Code in this form to inject into the site
-    const switch_function = 'function switch_theme() { var stylesheet = document.getElementById("dark-theme"); if (stylesheet) { stylesheet.parentNode.removeChild(stylesheet); theme_switch.innerText = low_brightness; } else { stylesheet = document.createElement("link"); stylesheet.setAttribute("id", "dark-theme"); stylesheet.setAttribute("href", "' + browser.runtime.getURL("darken_9gag.css") + '"); stylesheet.setAttribute("rel", "stylesheet"); stylesheet.setAttribute("type", "text/css"); document.getElementsByTagName("head")[0].appendChild(stylesheet); document.getElementsByClassName("background-white")[0].setAttribute("class", "background-dark"); theme_switch.innerText = high_brightness; }}'
+    const switch_function = 'const low_brightness = "🔅"; const high_brightness = "🔆"; function switch_theme() { var theme_switch = document.getElementById("theme-switch"); var stylesheet = document.getElementById("dark-theme"); if (stylesheet) { stylesheet.parentNode.removeChild(stylesheet); document.getElementsByClassName("background-dark")[0].setAttribute("class", "background-white"); theme_switch.innerText = low_brightness; } else { stylesheet = document.createElement("link"); stylesheet.setAttribute("id", "dark-theme"); stylesheet.setAttribute("href", "' + browser.runtime.getURL("darken_9gag.css") + '"); stylesheet.setAttribute("rel", "stylesheet"); stylesheet.setAttribute("type", "text/css"); document.getElementsByTagName("head")[0].appendChild(stylesheet); document.getElementsByClassName("background-white")[0].setAttribute("class", "background-dark"); theme_switch.innerText = high_brightness; }}'
     // Inject code (end of body)
     var switch_function_tag = document.createElement("script");
     switch_function_tag.id = "theme-switch-function";
@@ -18,14 +18,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.debug(switch_function_tag);
     document.body.appendChild(switch_function_tag);
 
-    const low_brightness = "🔅";
-    const high_brightness = "🔆";
     // Create the switch button
     var theme_switch = document.createElement("a");
-    theme_switch.setAttribute("style", "display:block; height:30px; width:30px; float:left; padding:9px 0; font-size:16pt;");
+    theme_switch.id = "theme-switch";
+    theme_switch.style = "display:block; height:30px; width:30px; float:left; padding:9px 0; font-size:16pt;";
     theme_switch.setAttribute("onclick", "switch_theme()");
-    theme_switch.setAttribute("href", "javascript:void(0)");
-    theme_switch.innerText = low_brightness;
+    theme_switch.href= "javascript:void(0)";
+    theme_switch.innerText = "🔅";
     // ... and add it to the site (in the header, next to the search)
     var wrapper = document.getElementsByClassName("function-wrap")[0];
     wrapper.insertBefore(theme_switch, wrapper.childNodes[0]);
