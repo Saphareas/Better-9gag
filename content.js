f23bd0e92782faf0f943039f8340c5873ebf4627
Fabian Große <saphareas@googlemail.com>
2018-10-23 09:49:42 +0200

    Moved style tweaks

--- content.js
+++ content.js
@@ -1,14 +1,3 @@
-/* #### General style tweaks #### */
-document.addEventListener("DOMContentLoaded", function() {
-    var head = document.getElementsByTagName("head")[0];
-    var style_tag = document.createElement("style");
-    // TODO: Move into ui_tweaks.css
-    var style = `
-        #jsid-header-user-menu-avatar>a { border-radius: 2px; }
-        .popup-menu.postpage-share { top: 43px; left: 164px; }`
-    style_tag.append(style);
-    head.append(style_tag);
-});
 /* ### Remove annoying paid video post from ora.tv ### */
 document.addEventListener("scroll", function() {
     // find it
