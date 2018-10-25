e5168bdc6a5690998a7a23e8f9b203fdf739bbbb
Fabian Große <saphareas@googlemail.com>
2018-10-25 21:03:16 +0200

    Fixed notifications for cross-browser use

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
-let manifest = browser.runtime.getManifest();
+﻿let manifest = function() {
+    try { return browser.runtime.getManifest(); } catch (e) { console.debug(e); }
+    try { return chrome.runtime.getManifest(); } catch (e) { console.debug(e); }
+}();
 
 function handleOnInstalled(details) {
     if (details.reason == "install") {
@@ -6,12 +9,10 @@ function handleOnInstalled(details) {
             type: "basic",
             title: "Thanks!",
             message: "Thanks for installing Better 9gag.",
+            iconUrl: "icons/icon-48.png"
         };
-        if (chrome) {
-            chrome.notifications.create(notificationOptions);
-        } else {
-            browser.notifications.create(notificationOptions);
-        }
+        try { browser.notifications.create(notificationOptions); } catch (e) { console.debug(e); }
+        try { chrome.notifications.create(notificationOptions); } catch (e) { console.debug(e); }
     }
     else if (details.reason == "update") {
         let notificationOptions = {
@@ -20,17 +21,12 @@ function handleOnInstalled(details) {
             message: `Better 9gag was updated. Here is what has changed:
                 • This notification was added
                 • The extension now removes the sticky button in the bottom right
-                • Under-the-hood changes in preparation for version 2.0`
+                • Under-the-hood changes in preparation for version 2.0`,
+            iconUrl: "icons/icon-48.png"
         };
-        if (chrome) {
-            chrome.notifications.create(notificationOptions);
-        } else {
-            browser.notifications.create(notificationOptions);
-        }
+        try { browser.notifications.create(notificationOptions); } catch (e) { console.debug(e); }
+        try { chrome.notifications.create(notificationOptions); } catch (e) { console.debug(e); }
     }
 }
-if (chrome) {
-    chrome.runtime.onInstalled.addListener(handleOnInstalled);
-} else {
-    browser.runtime.onInstalled.addListener(handleOnInstalled);
-}
+try { browser.runtime.onInstalled.addListener(handleOnInstalled); } catch (e) { console.debug(e); } 
+try { chrome.runtime.onInstalled.addListener(handleOnInstalled); } catch (e) { console.debug(e); }
