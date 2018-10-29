38d6efa60e48ad8e7b98ce0bd17f8c276caabca2
Fabian Große <saphareas@googlemail.com>
2018-10-29 21:09:53 +0100

    Pre-release 1.7
    changed version and changelog

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ function handleOnInstalled(details) {
         let notificationOptions = {
             type: "basic",
             title: "Thanks!",
-            message: "Thanks for installing Better 9gag.",
+            message: "Thank you for installing Better 9gag. If you like this extension, please consider giving it a good review. If you don't like it, feel free to tell me why.",
             iconUrl: "icons/icon-48.png"
         };
         browser.notifications.create(notificationOptions);
@@ -15,12 +15,14 @@ function handleOnInstalled(details) {
             type: "basic",
             title: `Version ${manifest.version} Changelog`,
             message: `Better 9gag was updated. Here is what has changed:
-• Removed own theme switching button; uses 9gags new, own button instead.
--> If you've used 9gag's night mode before this update and you're stuck between the two night modes, please disable this extension, on 9gag switch off the native night mode, and then reactivate this extension.
-• Simplified cross-browser stuff; this should give better performance.`,
+• Videos don't start playing, when you just scroll past them
+• No more YouTube posts
+• General improvements to the video controls stuff
+• Maybe better performance`,
             iconUrl: "icons/icon-48.png"
         };
         browser.notifications.create(notificationOptions);
     }
 }
+
 browser.runtime.onInstalled.addListener(handleOnInstalled);
