55b96445ea7a0e8c064c8d892b004396df1af27c
Fabian Große <saphareas@googlemail.com>
2018-12-03 11:03:16 +0100

    Fix for content scripts not running in Edge

--- app/lib/better9gag.js
+++ app/lib/better9gag.js
@@ -2,17 +2,19 @@ if (typeof browser == "undefined") {
     var browser = chrome;
 }
 
-function registerObserver(callback, observerConfig={ childList: true, subtree: true }, registeringEvent="DOMContentLoaded") {
-    document.addEventListener(registeringEvent, function() {
+function registerObserver(callback, observerConfig={ childList: true, subtree: true }) {
+    $(document).ready(function() {
         // Create and attach the observer
         let body_observer = new MutationObserver(callback);
         body_observer.observe(document.body, observerConfig);
-    });
+    },1);
 }
 
-function getSetting(key, arg, callback) {
-    browser.storage.local.get(null, function(item) {
-        let value = item.settings[key];
-        if (value === true) { callback(arg) }
-    });
+function getSetting(key, arg, callback, item=localStorage) {
+    setTimeout(function() {
+        browser.storage.local.get(null, function(item) {
+            let value = item.settings[key];
+            if (value === true) { callback(arg) }
+        });
+    },1);
 }
