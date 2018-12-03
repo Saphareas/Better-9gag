216ecb0f38a95af22030885d29ca52fb52c00f96
Fabian Große <saphareas@googlemail.com>
2018-12-03 11:40:13 +0100

    Fix NSFW unlock for Edge

--- app/components/nsfw-unlock.js
+++ app/components/nsfw-unlock.js
@@ -3,20 +3,23 @@
 function hasVideo(postId, parentNode) {
     const srcUrlBase = "https://img-9gag-fun.9cache.com/photo/" + postId;
     // create a video node with the unlocked post as source
-    var video = document.createElement("video");
+    let video = document.createElement("video");
+    video.preload = "metadata";
     video.src = srcUrlBase + "_460sv.mp4";
     video.loop = true;
-    video.controls = true;
-    video.volume = 0.5;
     // inject it into the site asap
     video.onloadedmetadata = function() {
-        parentNode.appendChild(video);
+        post.parentNode.appendChild(video);
+        // finally remove the placeholder
+        post.parentNode.removeChild(post);
     }
-    // on error == if no video exists, replace it with the still image
+    // onerror == if no video exists, replace it with the still image
     video.onerror = function() {
         var image = document.createElement("img");
         image.src= srcUrlBase + "_460s.jpg";
-        parentNode.appendChild(image);
+        post.parentNode.appendChild(image);
+        // finally remove the placeholder
+        post.parentNode.removeChild(post);
     }
 }
 
@@ -26,13 +29,10 @@ function unlockNsfwPosts() {
     let nsfwPosts = document.getElementsByClassName("nsfw-post");
     // for each of them:
     for (let i = 0; i < nsfwPosts.length; i++) {
-        let parent = nsfwPosts[i].parentNode;
         // get the 9gag id
-        let postId = parent.parentNode.parentNode.parentNode.getAttribute("id").split('-')[2];
+        let postId = nsfwPosts[i].parentNode.parentNode.parentNode.parentNode.getAttribute("id").split('-')[2];
         // inject unlocked post
-        hasVideo(postId, parent);
-        // finally remove the placeholder
-        parent.removeChild(nsfwPosts[i]);
+        hasVideo(postId, nsfwPosts[i]);
     }
 }
 
